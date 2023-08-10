e8bc4b5df68594628fab4c4a503b6cdc5652e45b
Luan Vitor <luancamposck@gmail.com>
2023-08-10 00:06:05 +0000

    feat: create Search Context

---
+++ src/context/SearchProvider.tsx
@@ -0,0 +1,11 @@
+import { createContext } from "react"
+
+export const SearchContext = createContext('')
+
+const SearchProvider = (children: React.ReactNode ) => {
+    return (
+        <h1>
+
+        </h1>
+    )
+}
