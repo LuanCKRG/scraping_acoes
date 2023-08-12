ecea750df600f3edf9ede4ac069c88e338a4a896
Luan Vitor <luancamposck@gmail.com>
2023-08-12 19:05:42 +0000

    undefined: only tests

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,16 @@
-import axios from "axios"
-import {JSDOM } from "jsdom"
-import { NextResponse } from "next/server"
+import axios from "axios";
+import { JSDOM } from "jsdom";
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const { data } = await axios.get("https://www.safra.com.br/central-de-conteudo/analisar/analise-de-acoes-petrobras-petr4.htm" )
-  const dom = new JSDOM(data)
-  const result =  dom.window.document.querySelector(".sub")?.textContent
-  console.log(NextResponse.json(result))
-  return NextResponse.json(result)
-}
+  const { data } = await axios.get(
+    "https://www.safra.com.br/central-de-conteudo/analisar/analise-de-acoes-petrobras-petr4.htm"
+  );
+  const dom = new JSDOM(data);
+  const result = dom.window.document.querySelector(".sub")?.textContent;
+  const recomendations = ["neutra", "compra", "venda"];
+  const sim = "Uma flor";
+  console.log(sim.includes("Uma"));
+
+  return NextResponse.json(result);
+};
