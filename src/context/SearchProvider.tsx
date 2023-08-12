edccfefd59a668b5fa011121e7e539c6d6c93d93
Luan Vitor <luancamposck@gmail.com>
2023-08-12 15:13:32 +0000

    feat: update SearchProvider context

--- src/context/SearchProvider.tsx
+++ src/context/SearchProvider.tsx
@@ -1,11 +1,26 @@
-import { createContext } from "react"
+import { createContext, useContext, useState } from "react";
 
-export const SearchContext = createContext('')
-
-const SearchProvider = (children: React.ReactNode ) => {
-    return (
-        <h1>
-
-        </h1>
-    )
+interface SearchBarProps {
+  search: string;
+  handleSearch: () => void;
 }
+
+const SearchContext = createContext<SearchBarProps>({} as SearchBarProps);
+
+export const SearchProvider: React.FC = ({ children }: React.ReactNode) => {
+  const [search, setSearch] = useState<string>("");
+
+  function handleSearch(newValue: string) {
+    setSearch(newValue);
+  }
+
+  return (
+    <SearchContext.Provider value={{ search, handleSearch }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = () => {
+  return useContext(SearchContext);
+};
