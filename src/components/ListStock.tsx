8745f54cc0e407f1661f295ed150a4776a6f1194
LuanCKRG <crackersage@gmail.com>
2023-08-22 17:30:53 -0400

    feat: update ListStock component retrun a Stock component to each item on array

--- src/components/ListStock.tsx
+++ src/components/ListStock.tsx
@@ -1,9 +1,20 @@
+'use client'
+
+import { useStocks } from "@/context/SearchProvider"
 import { Stock } from "./Stock"
 
 export const ListStock = () => {
+  const { listOfStocks } = useStocks()
+
   return (
-    <div className="flex flex-col justify-center px-4">
-      <Stock />
+    <div className="flex flex-col space-y-4 justify-center sm:space-y-0 sm:grid sm:gap-4 sm:grid-cols-2 lg:gap-8 lg:grid-cols-3 px-4">
+      {listOfStocks.map(
+          ({price, recomendation, date, src, title, font}) => {
+
+            return <Stock price={price} recomendation={recomendation} src={src} date={date} title={title} font={font} /> 
+          }
+        )
+      }
     </div>
   )
 }
