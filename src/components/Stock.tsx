0edd98551380c07071d1224ef2b70dd3062c0752
LuanCKRG <crackersage@gmail.com>
2023-08-22 17:26:12 -0400

    feat: update Stock component

--- src/components/Stock.tsx
+++ src/components/Stock.tsx
@@ -1,7 +1,32 @@
-export const Stock = () => {
+import { SafraProps } from "@/types"
+
+export const Stock = ({title, recomendation, price, src, date, font}: SafraProps) => {
   return (
-    <section className="flex flex-col border-2 rounded-lg p-2 mb-8">
-      <h2 className="text-center">Title</h2>
+    <section className="flex flex-col border-2 rounded-lg p-2">
+      <div className="w-full border-b-2">
+        <h2 className="text-center">
+          <b>{title}</b>
+        </h2>
+      </div>
+      <div>
+        <div>
+          Preço-alvo: <b>{price}</b>
+        </div>
+        <div>
+          Recomendação: <b>{recomendation}</b>
+        </div>
+        <div>
+          Data: <b>{date}</b>
+        </div>
+        <div>
+          Orgão: <b>{font}</b>
+        </div>
+      </div>
+      <div className="mx-auto underline">
+        <a href={src}>
+          Fonte
+        </a>
+      </div>
     </section>
   )
 }
