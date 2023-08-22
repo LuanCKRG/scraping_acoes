ffddd0cd6459a36ec7f9c899d16c41a3d9c45967
LuanCKRG <crackersage@gmail.com>
2023-08-22 17:24:26 -0400

    refactor: rename SearchProvider to ListOfStocksProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-"use client"
-import { SearchProvider } from "@/context/SearchProvider"
+import { ListOfStocksProvider } from "@/context/SearchProvider"
 import { SearchBar } from "@/components/SearchBar"
 import { ListStock } from "@/components/ListStock"
 
-const Home = () => {
-  // const response = await fetch('http://localhost:3000/api')
-  // const user = await response.json()
+const Home =  () => {
 
   return (
-    <SearchProvider>
+    <ListOfStocksProvider>
       <div className="flex flex-col">
         <header className="text-center py-4 border-2 rounded-b-md">
           <h1 className="text-xl">Resumo</h1>
@@ -17,7 +14,7 @@ const Home = () => {
 
         <ListStock />
       </div>
-    </SearchProvider>
+    </ListOfStocksProvider>
   )
 }
 
