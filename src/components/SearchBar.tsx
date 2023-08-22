57203c1581c1a339879cfb30dbd12d8a6ea3e23e
LuanCKRG <crackersage@gmail.com>
2023-08-22 17:27:09 -0400

    feat: update SearchBar http is there now

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,24 @@
-import { useContext } from "react"
-import { useSearch } from "@/context/SearchProvider"
+'use client'
+
+import { useState } from "react"
+import { useStocks } from "@/context/SearchProvider"
 import { BiSearchAlt } from "react-icons/bi"
+import { ListOfStocksProps, SafraProps } from "@/types"
 
 export const SearchBar = () => {
-  const { handleSearch, search } = useSearch()
+  const [search, setSearch] = useState<string>('')
+  const { handleListOfStocks } = useStocks()
+
+  async function updateListOfStocks(){
+    try {
+      const result = await fetch(`http://localhost:3000/api/${search}`, {cache: 'no-store'})
+      const res: SafraProps[] = await result.json()
+      handleListOfStocks(res)
+    }
+    catch(e){
+      return
+    }
+  }
 
   return (
     <span className="flex justify-center items-center my-4 px-4">
@@ -12,10 +27,10 @@ export const SearchBar = () => {
           className="w-full p-2 bg-black rounded-l-lg focus:outline-none text-center"
           placeholder="Digite uma ação. Ex.: PETR4, GGBR4, SNAG11"
           value={search}
-          onChange={(e) => handleSearch(e.currentTarget.value)}
+          onChange={(e) => setSearch(e.currentTarget.value)}
           type="text"
         />
-        <span className="flex justify-center px-2 items-center rounded-r-lg">
+        <span onClick={() => updateListOfStocks()} className="flex justify-center cursor-pointer px-2 items-center rounded-r-lg">
           <BiSearchAlt size={30} />
         </span>
       </div>
