81a77534bfd7698309340bf2a32695b70c14baac
Luan Vitor <luancamposck@gmail.com>
2023-08-23 20:39:36 +0000

    Update 10 files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Resumo | Ações',
-  description: 'A web scraping by LuanCK',
+	title: "Resumo | Ações",
+	description: "A web scraping by LuanCK",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-br">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang="pt-br">
+			<body className={inter.className}>
+				{children}
+			</body>
+		</html>
+	)
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import { useState } from "react"
 import { useStocks } from "@/context/LisofStocksProvider"
@@ -6,34 +6,36 @@ import { BiSearchAlt } from "react-icons/bi"
 import { SafraProps } from "@/types"
 
 export const SearchBar = () => {
-  const [search, setSearch] = useState<string>('')
-  const { handleListOfStocks } = useStocks()
+	const [search, setSearch] = useState<string>("")
+	const { handleListOfStocks } = useStocks()
 
-  async function updateListOfStocks(){
-    try {
-      const result = await fetch(`https://ck-scraping-acoes.vercel.app/api/${search}`, {cache: 'no-store'})
-      const res: SafraProps[] = await result.json()
-      handleListOfStocks(res)
-    }
-    catch(e){
-      return
-    }
-  }
+	async function updateListOfStocks() {
+		try {
+			const result = await fetch(`/api/${search}`, { cache: "no-store" })
+			const res: SafraProps[] = await result.json()
+			handleListOfStocks(res)
+		} catch (e) {
+			return
+		}
+	}
 
-  return (
-    <span className="flex justify-center items-center my-4 px-4">
-      <div className="flex w-full max-w-xs sm:max-w-md border-white border-2 rounded-lg">
-        <input
-          className="w-full p-2 bg-black rounded-l-lg focus:outline-none text-center"
-          placeholder="Digite uma ação. Ex.: PETR4, GGBR4, SNAG11"
-          value={search}
-          onChange={(e) => setSearch(e.currentTarget.value)}
-          type="text"
-        />
-        <span onClick={() => updateListOfStocks()} className="flex justify-center cursor-pointer px-2 items-center rounded-r-lg">
-          <BiSearchAlt size={30} />
-        </span>
-      </div>
-    </span>
-  )
+	return (
+		<span className="flex justify-center items-center my-4 px-4">
+			<div className="flex w-full max-w-xs sm:max-w-md border-white border-2 rounded-lg">
+				<input
+					className="w-full p-2 bg-black rounded-l-lg focus:outline-none text-center"
+					placeholder="Digite uma ação. Ex.: PETR4, GGBR4, SNAG11"
+					value={search}
+					onChange={(e) => setSearch(e.currentTarget.value)}
+					type="text"
+				/>
+				<span
+					onClick={() => updateListOfStocks()}
+					className="flex justify-center cursor-pointer px-2 items-center rounded-r-lg"
+				>
+					<BiSearchAlt size={30} />
+				</span>
+			</div>
+		</span>
+	)
 }
